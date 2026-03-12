3d1e384e497605c6fa75008a808189ef2e2cc4d1
bethaneyms <bethaneym03@gmail.com>
2026-03-12 15:12:32 -0500

    Added float and scale
    Added scale to the moving background

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
 
-public GameObject bckPrefab;
-public float speed;
-private GameObject[] bcks;
-public float pivoitPoint;
-
 public class BackgroundManager : MonoBehaviour
 {
+    public GameObject bckPrefab;
+    public float speed;
+    private GameObject[] bcks;
+    public float pivotPoint;
+    public float scale;
+    
 
     void Start()
     {
+        pivotPoint = scale * 16 * -0.32f;
+        bckPrefab.transform.localScale = new Vector3(scale, scale, scale);
+
         bcks = new GameObject[3];
 
-        float xPos = pivoitPoint - (pivoitPoint / 2 * i);
-        float yPos = pivoitPoint - (pivoitPoint / 2 * i);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) 
+        {
+            float xPos = pivotPoint - (pivotPoint / 2 * i);
+            float yPos = pivotPoint - (pivotPoint / 2 * i);
+            Vector3 position = new Vector3(xPos, yPos, 0.0f);
+
             bcks[i] = Instantiate(bckPrefab, position, Quaternion.identity);
         }
-        Vector3 position = new Vector3(xPos, yPos, 0.0f);
     }
 
     void Update()
     {
-        
+        for (int i = 0; i < 3; i++) 
+        {
+            float xPos = bcks[i].transform.position.x + speed * Time.deltaTime;
+            float yPos = bcks[i].transform.position.y + speed * Time.deltaTime;
+            Vector3 position = new Vector3(xPos, yPos);
+            
+            if (bcks[i].transform.position.x < -pivotPoint/2){
+                position = new Vector3(pivotPoint, pivotPoint);
+            }
+        bcks[i].transform.position = position;
+        }
     }
 }
